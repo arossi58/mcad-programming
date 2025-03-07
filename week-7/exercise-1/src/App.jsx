8f556ae8a5ca31aa0cdedb550c0a46f76af6be96
arossi58 <31297871+arossi58@users.noreply.github.com>
2025-03-07 13:10:40 -0600

    trying to get images to work

--- week-7/exercise-1/src/App.jsx
+++ week-7/exercise-1/src/App.jsx
@@ -6,13 +6,13 @@ const animal = [
   {
       title:"Llama",
       trivia:"Llamas are effective guard animals because of their excellent vision and sense of smell. They are often used to guard herds of alpacas.", 
-      image:"./src/assets/cria.jpg",
+      image:"/public/cria.jpg",
       alt:"A cute llama looking at you" },
       
   {
       title:"Alpaca",
       trivia:"Around 6000 years ago the Andean people started to domesticate alpacas",
-      image:"./src/assets/alpaca.jpg",
+      image:"/public/alpaca.jpg",
       alt:"a group of alpacas in the mountains"
   }
 ];
